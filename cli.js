52942a4521d8ddf28fb7d9ce3c45d83af6bd1ac8
Rishi Giri <rushforlinux@gmail.com>
2017-01-19 05:17:29 +0530

    functions, better error handling

--- cli.js
+++ cli.js
@@ -19,20 +19,57 @@ const spinner = ora();
 const arg = process.argv[2];
 const pre = `${chalk.bold.cyan('›')} `;
 
-if (!arg || arg === '-h' || arg === '--help') {
+const showMessage = () => {
+	logUpdate();
+	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Quote of the day')}`;
+	spinner.start();
+	return;
+};
+
+const showError = () => {
+	logUpdate(`\n${pre} ${chalk.dim('Could not find the Quote. Please try again!')}\n`);
+	process.exit(1);
+};
+
+const showQuotes = arg => {
+	logUpdate();
+	console.log(`${pre}${arg}`);
+	console.log();
+	spinner.stop();
+	return;
+};
+
+const quoteOftheDay = () => {
+	return chalk.dim('Quote of the Day');
+};
+
+const dnsBrainy = () => {
+	dns.lookup('brainyquote.com', err => {
+		if (err) {
+			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
+			process.exit(1);
+		}
+	});
+};
+
+const args = ['-g', '--goodreads', '-b', '--brainyquote', '-e', '--eduro', '-l', '--love', '-a', '--art', '-n', '--nature', '-f', '--funny', '-v', '--version', '-s', '--source'];
+
+if (!arg || arg === '-h' || arg === '--help' || args.indexOf(arg) === -1) {
 	console.log(`
- ${chalk.bold.cyan('Usage:')} kote <command>
+ ${chalk.cyan('Usage:')} kote <command>
 
- ${chalk.bold.cyan('Command: ')}
-  -g, --goodreads        fetch quote of the day from goodread      ${chalk.bold.green('⚫')} 
-  -b, --brainyquote      fetch quote of the day from brainyquotes  ${chalk.bold.cyan('⚫')} 
-  -e, --eduro            quote of the day by eduro                 ${chalk.bold.red('⚫')} 
-  -l, --love             romantic quote of the day                 ${chalk.bold.cyan('⚫')} 
-  -a, --art              art quote of the day                      ${chalk.bold.cyan('⚫')} 
-  -n, --nature           nature quote of the day                   ${chalk.bold.cyan('⚫')} 
-  -f, --funny            funny quote of the day                    ${chalk.bold.cyan('⚫')} 
+ ${chalk.cyan('Command: ')}
+  -g, --goodreads        ${quoteOftheDay()} : GoodReads
+  -b, --brainyquote      ${quoteOftheDay()} : BrainyQuotes
+  -e, --eduro            ${quoteOftheDay()} : Eduro
 
- ${chalk.bold.cyan('Kote: ')}
+ ${chalk.cyan('Extra :')}
+  -l, --love             ${quoteOftheDay()} : Love
+  -a, --art              ${quoteOftheDay()} : Art
+  -n, --nature           ${quoteOftheDay()} : Nature
+  -f, --funny            ${quoteOftheDay()} : Funny
+
+ ${chalk.cyan('Kote: ')}
   -h, --help             display help
   -s, --soruce           extra information
   -v, --version          display kote's version
@@ -49,75 +86,62 @@ if (arg === '-s' || arg === '--source') {
 
 if (arg === '-g' || arg === '--goodreads') {
 	dns.lookup('goodreads.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
+		if (err) {
 			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
 			process.exit(1);
 		}
 	});
 	const url = 'https://www.goodreads.com/quotes_of_the_day';
-	logUpdate();
-	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Quote of the day')}`;
-	spinner.start();
+	showMessage();
 	got(url).then(res => {
 		const $ = cheerio.load(res.body);
 		let quote = $('.quoteText').text().trim();
 		quote = quote.split('//<!')[0].trim().split('―')[0].trim();
-		logUpdate();
-		console.log(`${pre}${quote}`);
-		console.log();
-		spinner.stop();
+		showQuotes(quote);
+	}).catch(err => {
+		if (err) {
+			showError();
+		}
 	});
 }
 
 if (arg === '-b' || arg === '--brainyquote') {
-	dns.lookup('brainyquote.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
-			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
-			process.exit(1);
-		}
-	});
+	dnsBrainy();
 	const url = 'http://www.brainyquote.com/quotes_of_the_day.html';
-	logUpdate();
-	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Quote of the day')}`;
-	spinner.start();
+	showMessage();
 	got(url).then(res => {
 		const $ = cheerio.load(res.body);
 		const quote = $('.bqQuoteLink').eq(0).text().trim();
-		logUpdate();
-		console.log(`${pre}“${quote}”`);
-		console.log();
-		spinner.stop();
+		showQuotes(`"${quote}"`);
+	}).catch(err => {
+		if (err) {
+			showError();
+		}
 	});
 }
 
 if (arg === '-e' || arg === '--eduro') {
 	dns.lookup('eduro.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
+		if (err) {
 			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
 			process.exit(1);
 		}
 	});
 	const url = 'http://www.eduro.com/';
-	logUpdate();
-	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Quote of the day')}`;
-	spinner.start();
+	showMessage();
 	got(url).then(res => {
 		const $ = cheerio.load(res.body);
 		const quote = $('.article dailyquote p').eq(0).text().trim();
-		logUpdate();
-		console.log(`${pre}“${quote}”`);
-		console.log();
-		spinner.stop();
+		showQuotes(`"${quote}"`);
+	}).catch(err => {
+		if (err) {
+			showError();
+		}
 	});
 }
 
 if (arg === '-l' || arg === '--love') {
-	dns.lookup('brainyquote.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
-			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
-			process.exit(1);
-		}
-	});
+	dnsBrainy();
 	const url = 'http://www.brainyquote.com/quotes_of_the_day.html';
 	logUpdate();
 	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Love quote of the day')}`;
@@ -125,20 +149,16 @@ if (arg === '-l' || arg === '--love') {
 	got(url).then(res => {
 		const $ = cheerio.load(res.body);
 		const quote = $('.bqQuoteLink').eq(1).text().trim();
-		logUpdate();
-		console.log(`${pre}“${quote}”`);
-		console.log();
-		spinner.stop();
+		showQuotes(`"${quote}"`);
+	}).catch(err => {
+		if (err) {
+			showError();
+		}
 	});
 }
 
 if (arg === '-a' || arg === '--art') {
-	dns.lookup('brainyquote.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
-			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
-			process.exit(1);
-		}
-	});
+	dnsBrainy();
 	const url = 'http://www.brainyquote.com/quotes_of_the_day.html';
 	logUpdate();
 	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Art quote of the day')}`;
@@ -146,20 +166,16 @@ if (arg === '-a' || arg === '--art') {
 	got(url).then(res => {
 		const $ = cheerio.load(res.body);
 		const quote = $('.bqQuoteLink').eq(2).text().trim();
-		logUpdate();
-		console.log(`${pre}“${quote}”`);
-		console.log();
-		spinner.stop();
+		showQuotes(`"${quote}"`);
+	}).catch(err => {
+		if (err) {
+			showError();
+		}
 	});
 }
 
 if (arg === '-n' || arg === '--nature') {
-	dns.lookup('brainyquote.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
-			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
-			process.exit(1);
-		}
-	});
+	dnsBrainy();
 	const url = 'http://www.brainyquote.com/quotes_of_the_day.html';
 	logUpdate();
 	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Nature quote of the day')}`;
@@ -167,20 +183,16 @@ if (arg === '-n' || arg === '--nature') {
 	got(url).then(res => {
 		const $ = cheerio.load(res.body);
 		const quote = $('.bqQuoteLink').eq(3).text().trim();
-		logUpdate();
-		console.log(`${pre}“${quote}”`);
-		console.log();
-		spinner.stop();
+		showQuotes(`"${quote}"`);
+	}).catch(err => {
+		if (err) {
+			showError();
+		}
 	});
 }
 
 if (arg === '-f' || arg === '--funny') {
-	dns.lookup('brainyquote.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
-			logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n`);
-			process.exit(1);
-		}
-	});
+	dnsBrainy();
 	const url = 'http://www.brainyquote.com/quotes_of_the_day.html';
 	logUpdate();
 	spinner.text = `${chalk.dim('Fetching')} ${chalk.bold('Funny quote of the day')}`;
@@ -188,13 +200,14 @@ if (arg === '-f' || arg === '--funny') {
 	got(url).then(res => {
 		const $ = cheerio.load(res.body);
 		const quote = $('.bqQuoteLink').eq(4).text().trim();
-		logUpdate();
-		console.log(`${pre}“${quote}”`);
-		console.log();
-		spinner.stop();
+		showQuotes(`"${quote}"`);
+	}).catch(err => {
+		if (err) {
+			showError();
+		}
 	});
 }
 
 if (arg === '--version' || arg === '-v') {
-	console.log(chalk.bold.cyan(`\n${pre}Current kote version:`, require('./package.json').version, `\n`));
+	console.log(`\n${pre}Current kote version:`, require('./package.json').version, `\n`);
 }
