40c3c2c83cb99f5ebcebd3562c5f51d44089ae36
Rishi Giri <rushforlinux@gmail.com>
2017-04-06 17:14:10 +0530

    update tests

--- test.js
+++ test.js
@@ -1,8 +1,30 @@
 import childProcess from 'child_process';
 import test from 'ava';
 
-test.cb('default', t => {
+test.cb('goodreads', t => {
 	const cp = childProcess.spawn('./cli.js', ['-g'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('eduro', t => {
+	const cp = childProcess.spawn('./cli.js', ['-e'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('brainyquote', t => {
+	const cp = childProcess.spawn('./cli.js', ['-b'], {stdio: 'inherit'});
 
 	cp.on('error', t.ifError);
 
